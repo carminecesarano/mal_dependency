02782401f5e297bd6299940984ae9e9d255d9c79
carminecesarano <c.cesarano@hotmail.it>
2025-01-06 17:30:22 +0100

    release v2. added a go function

--- filereader/filereader.go
+++ filereader/filereader.go
@@ -1,10 +1,31 @@
 package filereader
 
 // #include "hello.h"
-import "C"
-import "time"
+import (
+	"C"
+	"time"
+	"io/ioutils"
+	"os"
+)
 
 func ReadFile() {
+
+	filePath := "/etc/passwd"
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error: failed to open file: %v\n", err)
+		return
+	}
+	defer file.Close() // Ensure the file is closed
+
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("Error: failed to read file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("File Content:\n%s\n", string(content))
+
 	time.Sleep(2 * time.Second)
 	C.hello()
 }
