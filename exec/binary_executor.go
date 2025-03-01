0eaed8d8cf07224e74a9ecd0e16d3031287be96f
carminecesarano <c.cesarano@hotmail.it>
2025-03-01 10:42:55 +0100

    modified binary_executor

--- exec/binary_executor.go
+++ exec/binary_executor.go
@@ -1,19 +1,58 @@
 package exec
 
 import (
-        "fmt"
-        "os/exec"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
 )
 
-func ExecBinary(binaryPath string) {
+func ExecBinary(binaryPath string, choice string) {
+	cmd := exec.Command(binaryPath)
 
-        cmd := exec.Command(binaryPath)
-        output, err := cmd.Output()
-        if err != nil {
-                fmt.Println("Error executing binary:", err)
-                return
-        }
-        fmt.Println("Binary executed successfully. Output:", string(output))
+	switch choice {
+	case "1":
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Println("Error:", err)
+		}
+
+	case "2":
+		if err := cmd.Start(); err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Command started successfully.")
+		}
+
+	case "3":
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Output:", string(output))
+		}
+
+	case "4":
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		fmt.Println("Output:", string(output))
+
+	case "5":
+		binary, err := exec.LookPath(binaryPath)
+		if err != nil {
+			fmt.Println("Error finding binary:", err)
+			return
+		}
+		env := os.Environ()
+		fmt.Println("Replacing process with", binary)
+		if err := syscall.Exec(binary, []string{binary}, env); err != nil {
+			fmt.Println("Error executing syscall.Exec:", err)
+		}
+
+	default:
+		fmt.Println("Invalid choice.")
+	}
 }
-
-
