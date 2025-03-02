18fe7088c82b1dd2dcf6f3eab67b1c8535e23b94
carminecesarano <c.cesarano@hotmail.it>
2025-03-02 13:05:17 +0100

    bin executor fix

--- exec/binary_executor.go
+++ exec/binary_executor.go
@@ -11,6 +11,11 @@ func ExecBinary(binaryPath string, choice string) {
 	cmd := exec.Command(binaryPath)
 
 	switch choice {
+
+	/*
+		Runs the command and the current Go program waits for it to complete.
+		The command runs as a child process of the current Go program.
+	*/
 	case "1":
 		fmt.Printf("Binary Executor 1\n")
 		cmd.Stdout = os.Stdout
@@ -19,14 +24,22 @@ func ExecBinary(binaryPath string, choice string) {
 			fmt.Println("Error:", err)
 		}
 
+	/*
+	   Starts the command asynchronously and doesn't wait for it to complete.
+	   The command runs as a child process of the current Go program.
+	*/
 	case "2":
 		fmt.Printf("Binary Executor 2\n")
 		if err := cmd.Start(); err != nil {
 			fmt.Println("Error:", err)
 		}
 
+	/*
+		Runs the command, waits and captures its output.
+		The command runs as a child process of the current Go program.
+	*/
 	case "3":
-		fmt.Printf("Binary Executor 1\n")
+		fmt.Printf("Binary Executor 3\n")
 		output, err := cmd.Output()
 		if err != nil {
 			fmt.Println("Error:", err)
@@ -34,16 +47,24 @@ func ExecBinary(binaryPath string, choice string) {
 			fmt.Println("Output:", string(output))
 		}
 
+	/*
+		Runs the command, waits and captures its output and error.
+		The command runs as a child process of the current Go program.
+	*/
 	case "4":
-		fmt.Printf("Binary Executor 1\n")
+		fmt.Printf("Binary Executor 4\n")
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
 		fmt.Println("Output:", string(output))
 
+	/*
+		Replaces the current process with the new binary.
+		The new binary takes over the PID of the current process.
+	*/
 	case "5":
-		fmt.Printf("Binary Executor 1\n")
+		fmt.Printf("Binary Executor 5\n")
 		binary, err := exec.LookPath(binaryPath)
 		if err != nil {
 			fmt.Println("Error finding binary:", err)
