3b6fec1fcfd4423cdd3830ade679ff25b73980c3
carminecesarano <c.cesarano@hotmail.it>
2025-03-28 18:38:54 +0100

    modified path for injection points  log

--- instrumentation/injection_points.go
+++ instrumentation/injection_points.go
@@ -13,7 +13,7 @@ func LogInjectionPoint() {
 	injectionID := fmt.Sprintf("%s:%d:%s", file, line, fn.Name())
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
 	logMessage := fmt.Sprintf("[%s] Injection triggered: %s", timestamp, injectionID)
-	f, _ := os.OpenFile("./injection_points.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, _ := os.OpenFile("/home/carmine/projects/workspace_goleash/injection_points.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer f.Close()
 	fmt.Fprintln(f, logMessage)
 }
