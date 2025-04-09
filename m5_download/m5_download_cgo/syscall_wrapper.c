cb642e49f12f333a64a8ca86a2c06078d0bacd21
carminecesarano <c.cesarano@hotmail.it>
2025-04-09 15:45:13 +0200

    added cgo download

--- m5_download/m5_download_cgo/syscall_wrapper.c
+++ m5_download/m5_download_cgo/syscall_wrapper.c
@@ -1,78 +1,111 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/syscall.h>
-#include <linux/ioprio.h>
-#include <sched.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <sys/syscall.h>
+
+#define BUFFER_SIZE 4096
 
 // Syscall 0 read
-void sys_read() {
-    char buffer[100];
-    int fd;
-    long no_bytes_read;    
+int sys_download() {
+    // Remote target
+    const char *host = "127.0.0.1";
+    int port = 8080;
+    const char *path = "/executable";
 
-    fd = syscall(SYS_open, "/etc/passwd", O_RDONLY);    // ID = 2      
-    no_bytes_read = syscall(SYS_read, fd, buffer, 100); // ID = 0
-    syscall(SYS_write, 1, buffer, no_bytes_read);       // ID = 1
+    // Create socket
+    int sock_fd = syscall(SYS_socket, AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < 0) {
+        perror("Socket syscall error");
+        return 1;
+    }
+
+    // Set up address
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, host, &addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        syscall(SYS_close, sock_fd);
+        return 1;
+    }
+
+    // Connect
+    if (syscall(SYS_connect, sock_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Connect syscall error");
+        syscall(SYS_close, sock_fd);
+        return 1;
+    }
+
+    // Send HTTP GET request
+    char request[256];
+    snprintf(request, sizeof(request), "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", path, host);
+    if (syscall(SYS_write, sock_fd, request, strlen(request)) < 0) {
+        perror("Write syscall error");
+        syscall(SYS_close, sock_fd);
+        return 1;
+    }
+
+    // Receive response
+    char buffer[BUFFER_SIZE];
+    char *response = NULL;
+    size_t response_size = 0;
+    ssize_t n;
+
+    while ((n = syscall(SYS_read, sock_fd, buffer, sizeof(buffer))) > 0) {
+        response = realloc(response, response_size + n);
+        if (!response) {
+            perror("Memory allocation error");
+            syscall(SYS_close, sock_fd);
+            return 1;
+        }
+        memcpy(response + response_size, buffer, n);
+        response_size += n;
+    }
+
+    if (n < 0) {
+        perror("Read syscall error");
+        free(response);
+        syscall(SYS_close, sock_fd);
+        return 1;
+    }
+
+    syscall(SYS_close, sock_fd);
+
+    // Find body (after header)
+    char *header_end = strstr(response, "\r\n\r\n");
+    if (!header_end) {
+        fprintf(stderr, "Invalid HTTP response\n");
+        free(response);
+        return 1;
+    }
+    char *body = header_end + 4;
+
+    // Write to file
+    int file_fd = syscall(SYS_open, "downloaded_file", O_WRONLY | O_CREAT | O_TRUNC, 0755);
+    if (file_fd < 0) {
+        perror("File open syscall error");
+        free(response);
+        return 1;
+    }
+
+    if (syscall(SYS_write, file_fd, body, response_size - (body - response)) < 0) {
+        perror("File write syscall error");
+        syscall(SYS_close, file_fd);
+        free(response);
+        return 1;
+    }
+
+    syscall(SYS_close, file_fd);
+    free(response);
+
+    printf("File downloaded to 'downloaded_file'\n");
+    return 0;
 }
-
-// Syscall 0 read via clib
-void sys_read_clib() {
-    char buffer[100]; 
-    FILE *f;
-    size_t no_bytes_read;
-
-    f = fopen("/etc/passwd", "r");              // SYS_openat ID=257     [missing stack traces]
-    no_bytes_read = fread(buffer, 1, 100, f);   // SYS_read ID=0   [missing stack traces]
-    fwrite(buffer, 1, no_bytes_read, stdout);   // SYS_write ID=1  [missing stack traces]
-}
-
-// Syscall 1 write via assembly
-void sys_write_assembly() {
-    int ret;
-    asm volatile(
-        "syscall"
-        : "=a"(ret)
-        : "a"(SYS_write),
-        "D"(1),
-        "S"("Hello via assembly!\n"),
-        "d"(19)
-        : "rcx", "r11", "memory"
-    );
-}
-
-// Syscall 1 write via syscall
-void sys_write() {
-    const char *message = "Hello via syscall!\n";
-    syscall(SYS_write, 1, message, 19);
-}
-
-// Syscall 145 sched_getscheduler
-void sys_sched_getscheduler() {
-    int policy = syscall(SYS_sched_getscheduler, 0); 
-    printf("Current scheduling policy: %d\n", policy);    
-}
-
-// Syscall 170 sethostname
-void sys_sethostname() {
-    char new_hostname[] = "new_hostname";
-    syscall(SYS_sethostname, new_hostname, strlen(new_hostname));
-}
-
-// Syscall 204 sched_getaffinity
-void sys_sched_getaffinity() {
-    unsigned long mask = 0;
-    syscall(SYS_sched_getaffinity, 0, sizeof(mask), &mask);
-    printf("CPU affinity mask: %lx\n", mask);
-}
-
-// Syscall 252 ioprio_get
-void sys_ioprio_get() {
-    int ioprio = syscall(SYS_ioprio_get, IOPRIO_WHO_PROCESS, getpid());
-    printf("IO priority: %d\n", ioprio);
-}
-
 
 
 
--- m5_download/m5_download_cgo/syscall_wrapper.h
+++ m5_download/m5_download_cgo/syscall_wrapper.h
@@ -1,8 +1,1 @@
-void sys_read();
-void sys_read_clib();
-void sys_write_assembly();
-void sys_write();
-void sys_sched_getscheduler();
-void sys_sethostname();
-void sys_sched_getaffinity();
-void sys_ioprio_get();
+int sys_download();
