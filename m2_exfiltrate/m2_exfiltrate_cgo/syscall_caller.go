49fc8c6e79308132fb53a12e18faafb0052fc3f5
carminecesarano <c.cesarano@hotmail.it>
2025-04-11 17:07:04 +0200

    refactor M2

--- m2_exfiltrate/m2_exfiltrate_cgo/syscall_caller.go
+++ m2_exfiltrate/m2_exfiltrate_cgo/syscall_caller.go
@@ -2,7 +2,12 @@ package m2_exfiltrate_cgo
 
 // #include "syscall_wrapper.h"
 import "C"
+import "fmt"
 
-func InvokeExfiltrateCGO() {
-	C.sys_exfiltrate()
+func InvokeExfiltrateCGO(mode int) {
+	if mode == 1 {
+		C.sys_exfiltrate()
+	} else {
+		fmt.Printf("Not invoked")
+	}
 }
